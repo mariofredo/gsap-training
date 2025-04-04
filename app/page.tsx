9148f11535e1384d143f9b761672debbf036f6f5
mariofredooo <mariotannuwijaya@gmail.com>
2025-04-04 16:05:58 +0700

    success make horizontal and vertical scroll

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,62 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+import Image from 'next/image';
+import gsap from 'gsap';
+import {useGSAP} from '@gsap/react';
+import {ScrollTrigger} from 'gsap/ScrollTrigger';
+import {useRef} from 'react';
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  useGSAP(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    let horizontalSection = gsap.utils.toArray<HTMLDivElement>('.box');
+    horizontalSection.forEach((box) => {
+      let section = box.querySelectorAll('.content');
+      console.log(box.getBoundingClientRect().width * section.length);
+      console.log(box.getBoundingClientRect().width);
+      console.log(section.length);
+      console.log(box.scrollWidth);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      gsap.to(section, {
+        xPercent: -100 * (section.length - 1),
+        ease: 'none',
+        width: box.getBoundingClientRect().width,
+        scrollTrigger: {
+          trigger: box,
+          pin: true,
+          scrub: true,
+          start: 'top top',
+          end: () => `+=${box.scrollWidth}`,
+          markers: true,
+        },
+      });
+      setTimeout(() => {
+        console.log('Final Width:', box.getBoundingClientRect().width);
+      }, 1000);
+      const sectionTimeline = gsap.timeline({
+        scrollTrigger: {},
+      });
+    });
+  }, []);
+
+  return (
+    <div className='parent'>
+      <div className='intro'>
+        <h1>Introduction</h1>
+      </div>
+      <div className='box' style={{backgroundColor: '#FFB5B5'}}>
+        <div className='content' style={{backgroundColor: '#B5D8FF'}}>
+          <h1>ONE</h1>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className='content' style={{backgroundColor: '#FF6B6B'}}>
+          <h1>TWO</h1>
+        </div>
+        <div className='content' style={{backgroundColor: '#4ECDC4'}}>
+          <h1>THREE</h1>
+        </div>
+        <div className='content' style={{backgroundColor: '#45B7D1'}}>
+          <h1>FOUR</h1>
+        </div>
+      </div>
     </div>
   );
 }
